2e966643700dc82e51ea5777147874ada0f644e5
kthss01 <kthss01@naver.com>
2026-03-05 11:00:02 +0900

    Adjust code runner templates to hide answers and include function params

--- src/algorithms/p003_top_k_frequent.js
+++ src/algorithms/p003_top_k_frequent.js
@@ -3,7 +3,7 @@
 // 1) 숫자별 빈도수를 Map으로 계산하세요.
 // 2) min-heap 또는 bucket 전략으로 상위 k개를 추출하세요.
 
-function topKFrequent() {
+function topKFrequent(nums, k) {
   throw new Error('TODO: topKFrequent를 구현하세요. (정답 예시: src/answer/p003_top_k_frequent.answer.js)');
 }
 
